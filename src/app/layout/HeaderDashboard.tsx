335cdea96dcf55635deb2f8136bafdb7da98ccbb
Abdellah DAMRI <damriabdellah@gmail.com>
2024-02-07 21:59:35 +0100

    style: re-styling the header and adding dashboard header

--- src/app/layout/HeaderDashboard.tsx
+++ src/app/layout/HeaderDashboard.tsx
@@ -2,9 +2,9 @@ import { menus } from "@/data"
 import { Link, NavLink } from "react-router-dom"
 import { Logo } from "@/shared"
 
-export const Header = () => {
+export const HeaderDashboard = () => {
   return (
-    <nav className="sticky z-[100] left-0 top-0 w-full py-5 text-primary">
+    <nav className="sticky z-[100] left-0 top-0 w-full py-5 text-gray-700">
       <div className="container flex w-full items-center justify-between">
         <Link to={"/"}>
           <Logo />
@@ -14,7 +14,7 @@ export const Header = () => {
             <li key={menu?.id}>
               <NavLink
                 className={({ isActive }) =>
-                  `relative hover:rounded inline-flex overflow-hidden before:absolute before:content-[''] before:left-0 before:top-0 before:w-full before:h-full before:bg-primary before:-z-[1] before:transition-all transition-all duration-500 before:duration-500 hover:border-primary hover:text-secondary hover:before:translate-y-0 py-2 px-5 font-medium ${isActive ? "before:translate-y-[calc(100%-4px)]" : "before:translate-y-full"}`
+                  `relative hover:rounded inline-flex overflow-hidden before:absolute before:content-[''] before:left-0 before:top-0 before:w-full before:h-full before:bg-gray-700 before:-z-[1] before:transition-all transition-all duration-500 before:duration-500 hover:border-gray-700 hover:text-gray-300 hover:before:translate-y-0 py-2 px-5 font-medium ${isActive ? "before:translate-y-[calc(100%-4px)]" : "before:translate-y-full"}`
                 }
                 to={menu.path}
               >
